91a65b6c997fff97c419464df680248228f0b19e
LudyLUDK <88024622+LudyLUDK@users.noreply.github.com>
2024-02-26 12:55:54 -0300

    inicio

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const Messmagic = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+Messmagic.abilities = {
+  str: 'Messmagic.Ability.Str.long',
+  dex: 'Messmagic.Ability.Dex.long',
+  con: 'Messmagic.Ability.Con.long',
+  int: 'Messmagic.Ability.Int.long',
+  wis: 'Messmagic.Ability.Wis.long',
+  cha: 'Messmagic.Ability.Cha.long',
+};
+
+Messmagic.abilityAbbreviations = {
+  str: 'Messmagic.Ability.Str.abbr',
+  dex: 'Messmagic.Ability.Dex.abbr',
+  con: 'Messmagic.Ability.Con.abbr',
+  int: 'Messmagic.Ability.Int.abbr',
+  wis: 'Messmagic.Ability.Wis.abbr',
+  cha: 'Messmagic.Ability.Cha.abbr',
+};
